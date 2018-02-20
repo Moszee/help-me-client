bdd48c78eab30f9bbdfa7a70cbdacc43d50e88d3
mateusz.szpak <mateusz.szpak@ocado.com>
2018-02-20 21:33:18 +0100

    normalize css added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'resources/styles/index.css';
 import App from 'components/App';
+import 'normalize.css';
 import registerServiceWorker from 'components/util/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
